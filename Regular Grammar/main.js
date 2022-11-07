a8fe104b8c3ca6825ef4d56d0baa0d492cbed58b
GuiBatalhoti <guibatalhoti@gmail.com>
2022-11-07 16:27:03 -0300

    Limitando as regras para GRD

--- Regular Grammar/main.js
+++ Regular Grammar/main.js
@@ -1,29 +1,64 @@
+//importando as bibliotecas
 var imported = document.createElement('script');
 imported.src = './earley-oop.min.js';
 document.head.appendChild(imported);
 
+const regex_gramatica_direita = new RegExp("^[A-Z]->([a-z]*|[0-9]*)([A-Z]{0,1})$");
+
 function testGrammar(){
-    let rawStates = document.getElementById('regras_gramatica').value.replace(/([A-Z])/g, ' $1');
-    let states = rawStates.split(/\r?\n/);
+    let states = pegaRegras()
+
+    //pegando o input de teste
     let testInput = document.getElementById('inputGrammar').value;
     let token = testInput.split('');
-    console.log(rawStates);
-    console.log(states);
     
+    // biblioteca de gramática
     let gramatica = new tinynlp.Grammar(states);
 
+    // definindo a raiz
     const raiz = 'S';
+
+    // contruindo o analizador
     let saida = tinynlp.parse(
         token,
         gramatica,
         raiz
     );
     
+    // análise finalizadas
     let estado = saida.getFinishedRoot(raiz);
-
+    
+    // verificando s eo input é válido ou não
     let input = document.getElementById('inputGrammar');
     mudaCorTeste(input, estado);
+}
 
+function testeRegrasProducao(){
+    let states = pegaRegras();
+
+    let inputRegras = document.getElementById('regras_gramatica')
+    for (let estado of states){
+        mudaCorTeste(inputRegras, regex_gramatica_direita.test(estado.split(' ').join('')))
+        console.log(estado.split(' ').join(''))
+    }
+}
+
+function pegaRegras(){
+    /*
+     * Pegando o input das regras de produção,
+     * trocando as letras maíusculas por "*espaço* Letra", exemplo "A" para " A".    
+     */
+    let rawStates = document.getElementById('regras_gramatica').value.trim();
+    rawStates = rawStates.split(' ').join('')
+    rawStates = rawStates.replace(/([A-Z])/g, ' $1');
+    //imprimindo os inputs
+    console.log(rawStates);
+
+    //separando por linhas
+    let states = rawStates.split(/\r?\n/);
+    console.log(states);
+
+    return states;
 }
 
 
